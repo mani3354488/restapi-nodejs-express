1b9549f15499af93e2576c0dac83737188eea1b2
mani3354488 <mani3354488@gmail.com>
2020-10-28 19:11:22 +0530

    final commit

--- routes/sub.js
+++ routes/sub.js
@@ -1,75 +1,75 @@
 const express = require('express')
-const subscriber = require('../models/subscriber')
 const router = express.Router()
 const Subscriber = require('../models/subscriber')
 
-//getting all subs
-router.get('/', getSubscriber, async (req, res) => {
-    try {
-        const subscribers = await Subscriber.find()
-    } catch (err) {
-        res.status(500).json({message: err.message})
-    }
+// Getting all
+router.get('/', async (req, res) => {
+  try {
+    const subscribers = await Subscriber.find()
+    res.json(subscribers)
+  } catch (err) {
+    res.status(500).json({ message: err.message })
+  }
 })
 
-//getting one sub
+// Getting One
 router.get('/:id', getSubscriber, (req, res) => {
-   res.send(res.subscriber) 
+  res.json(res.subscriber)
 })
 
-//creating a sub
+// Creating one
 router.post('/', async (req, res) => {
-    const subscriber = new Subscriber({
-        name: req.body.name,
-        subscribedToChannel: req.body.subscribedToChannel
-    })
-
-    try{
-        const newSubscriber = await subscriber.save()
-        res.status(201).json(newSubscriber)
-    } catch (err) {
-        res.status(400).json({message: err.message})
-    }
+  const subscriber = new Subscriber({
+    name: req.body.name,
+    subscribedToChannel: req.body.subscribedToChannel
+  })
+  try {
+    const newSubscriber = await subscriber.save()
+    res.status(201).json(newSubscriber)
+  } catch (err) {
+    res.status(400).json({ message: err.message })
+  }
 })
 
-//updating a sub
-router.patch('/', getSubscriber, async (req, res) => {
-    if (req.body.name != null) {
-        res.subscriber.name = req.body.name
-    }
-    if (req.body.subscribedToChannel != null ) {
-        res.subscriber.subscribedToChannel = req.body.subscribedToChannel
-    }
-    try {
-        const updatedSubscriber = await res.subscriber.save()
-        res.json(updatedSubscriber)
-    } catch (err) {
-        res.status(400).json({message: err.message })
-    }
+// Updating One
+router.patch('/:id', getSubscriber, async (req, res) => {
+  if (req.body.name != null) {
+    res.subscriber.name = req.body.name
+  }
+  if (req.body.subscribedToChannel != null) {
+    res.subscriber.subscribedToChannel = req.body.subscribedToChannel
+  }
+  try {
+    const updatedSubscriber = await res.subscriber.save()
+    res.json(updatedSubscriber)
+  } catch (err) {
+    res.status(400).json({ message: err.message })
+  }
 })
 
-//deleting a sub
+// Deleting One
 router.delete('/:id', getSubscriber, async (req, res) => {
-    try{
-        await res.subscriber.remove()
-        res.json({ message: 'deleted' })
-    }catch (err) {
-        res.status(500).json({message: err.message})
-    }
+  try {
+    await res.subscriber.remove()
+    res.json({ message: 'Deleted Subscriber' })
+  } catch (err) {
+    res.status(500).json({ message: err.message })
+  }
 })
 
 async function getSubscriber(req, res, next) {
-    let subscriber
-    try{
-        subscriber = await Subscriber.findById(req.params.id)
-        if (subscriber == null) {
-            return res.status(404).json({message: 'cannot find sub'})
-        }
-    } catch (err) {
-        return res.status(500).json({ message: err.message })
+  let subscriber
+  try {
+    subscriber = await Subscriber.findById(req.params.id)
+    if (subscriber == null) {
+      return res.status(404).json({ message: 'Cannot find subscriber' })
     }
-    res.subscriber = subscriber
-    next()
+  } catch (err) {
+    return res.status(500).json({ message: err.message })
+  }
+
+  res.subscriber = subscriber
+  next()
 }
 
 module.exports = router
